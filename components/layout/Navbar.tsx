8d7d200661f73978fb8085577fc02a94e46a7120
moxi-edtech <fornet069@gmail.com>
2026-09-25 10:30:35 -0300

    Refactor: add components/layout/Navbar.tsx

---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,13 @@
+export function Navbar() {
+  return (
+    <nav>
+      <a className="brand" href="#">DC.</a>
+      <div className="navlinks">
+        <a href="#work">Work</a>
+        <a href="#expertise">Expertise</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+  );
+}
